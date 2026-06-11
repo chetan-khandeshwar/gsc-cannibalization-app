d1f60572c60b729963668e41681a066cfb75bf4c
Chetan <aptngpchetan@gmail.com>
2026-06-11 02:11:59 -0700

    Fix redirect URI

--- auth.py
+++ auth.py
@@ -3,26 +3,28 @@ import streamlit as st
 from streamlit_oauth import OAuth2Component
 
 SCOPES = [
-    "https://www.googleapis.com/auth/webmasters.readonly"
+"https://www.googleapis.com/auth/webmasters.readonly"
 ]
 
+REDIRECT_URI = (
+"https://gsc-cannibalization-app-7mi43bptxgtjaajeeanxhp.streamlit.app"
+)
 
 def google_login():
 
-    oauth2 = OAuth2Component(
-        st.secrets["GOOGLE_CLIENT_ID"],
-        st.secrets["GOOGLE_CLIENT_SECRET"],
-        "https://accounts.google.com/o/oauth2/v2/auth",
-        "https://oauth2.googleapis.com/token"
-    )
+```
+oauth2 = OAuth2Component(
+    st.secrets["GOOGLE_CLIENT_ID"],
+    st.secrets["GOOGLE_CLIENT_SECRET"],
+    "https://accounts.google.com/o/oauth2/v2/auth",
+    "https://oauth2.googleapis.com/token"
+)
 
-    result = oauth2.authorize_button(
-        "Login with Google",
-        redirect_uri=st.query_params.get(
-            "redirect_uri",
-            ""
-        ),
-        scope=" ".join(SCOPES)
-    )
+result = oauth2.authorize_button(
+    "Login with Google",
+    redirect_uri=REDIRECT_URI,
+    scope=" ".join(SCOPES)
+)
 
-    return result
+return result
+```
